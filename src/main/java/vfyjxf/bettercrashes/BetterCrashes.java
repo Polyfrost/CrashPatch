92c39814c2513bec3c91e1615cc484ef5c2ad23d
vfyjxf <vfyjxf@outlook.com>
2021-07-14 18:42:20 +0800

    Fix bug

--- src/main/java/vfyjxf/bettercrashes/BetterCrashes.java
+++ src/main/java/vfyjxf/bettercrashes/BetterCrashes.java
@@ -6,12 +6,11 @@ import org.apache.logging.log4j.Logger;
 
 import cpw.mods.fml.common.Mod;
 
-@Mod(modid = BetterCrashes.MODID, version = BetterCrashes.VERSION, name = BetterCrashes.NAME, dependencies = BetterCrashes.DEPENDENCIES)
+@Mod(modid = BetterCrashes.MODID, version = BetterCrashes.VERSION, name = BetterCrashes.NAME)
 public class BetterCrashes {
     public static final String MODID = "bettercrashes";
     public static final String NAME = "BetterCrashes";
     public static final String VERSION = "@VERSION@";
-    public static final String DEPENDENCIES = "required-after:grimoire@[3.2.3,]";
 
     public static final Logger logger = LogManager.getLogger("BetterCrashes");
 }
