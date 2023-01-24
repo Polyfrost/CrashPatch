730d35c599f76acbf1a16333d0b6dc1cf9e42083
Caledonian <caledonianeh@gmail.com>
2023-01-23 22:37:07 -0800

    Edit config for better UX & add crashpatch logo

--- src/main/kotlin/cc/woverflow/crashpatch/config/CrashPatchConfig.kt
+++ src/main/kotlin/cc/woverflow/crashpatch/config/CrashPatchConfig.kt
@@ -1,50 +1,79 @@
 package cc.woverflow.crashpatch.config
 
 import cc.polyfrost.oneconfig.config.Config
-import cc.polyfrost.oneconfig.config.annotations.Dropdown
-import cc.polyfrost.oneconfig.config.annotations.Slider
-import cc.polyfrost.oneconfig.config.annotations.Switch
+import cc.polyfrost.oneconfig.config.annotations.*
+import cc.polyfrost.oneconfig.config.data.InfoType
 import cc.polyfrost.oneconfig.config.data.Mod
 import cc.polyfrost.oneconfig.config.data.ModType
+import cc.polyfrost.oneconfig.libs.universal.UDesktop.browse
+import java.net.URI
 
-object CrashPatchConfig : Config(Mod("CrashPatch", ModType.UTIL_QOL), "crashpatch.json") {
-    @Switch(
-        name = "Enable Initialization Crash Patch",
-        description = "Catch crashes during initialization and display a message instead of crashing the game."
-    )
-    var initCrashPatch = true
 
+object CrashPatchConfig : Config(Mod("CrashPatch", ModType.UTIL_QOL, "/assets/crashpatch/crashpatch_dark.svg"), "crashpatch.json") {
+
+    // Toggles
     @Switch(
-        name = "Enable In-Game Crash Patch",
-        description = "Catch crashes during gameplay and display a message instead of crashing the game."
+        name = "Catch crashes during gameplay",
+        description = "Catch crashes whilst in-game, and prevent the game from closing",
+        subcategory = "Patches"
     )
     var inGameCrashPatch = true
 
     @Switch(
-        name = "Enable Disconnect Crash Patch",
-        description = "Display a message when a reason is found for a disconnect."
+        name = "Patch crashes during launch",
+        description = "Catch crashes during initialization, & display a message.",
+        subcategory = "Patches"
+    )
+    var initCrashPatch = true
+
+    @Switch(
+        name = "Display disconnection causes",
+        description = "Display a message when a reason is found for a disconnect.",
+        subcategory = "Patches"
     )
     var disconnectCrashPatch = true
+
+    // Limits
+    @Info(
+        text = "It's recommended to restart your game after every few crashes, to avoid severe instability",
+        type = InfoType.WARNING,
+        size = 2,
+        subcategory = "Limits"
+    )
+    var ignored: Boolean = false
+
 
     @Slider(
         name = "Crash Limit",
         min = 1f,
-        max = 100f
+        max = 20f,
+        step = 1,
+        subcategory = "Limits"
     )
-    var crashLimit = 10
+    var crashLimit = 5
 
     @Switch(
         name = "Deobfuscate Crash Log",
-        description = "Deobfuscate the crash log."
+        description = "Makes certain class names more readable through deobfuscation",
+        size = 2,
+        subcategory = "Logs"
     )
     var deobfuscateCrashLog = true
 
     @Dropdown(
-        name = "Crash Log Upload Method",
+        name = "Log uploader",
         description = "The method used to upload the crash log.",
-        options = ["hst.sh", "mclo.gs (Aternos)"]
+        options = ["hst.sh (Default)", "mclo.gs (Aternos)"],
+        subcategory = "Logs"
     )
     var crashLogUploadMethod = 0
+
+    @Button(
+        name = "Polyfrost support",
+        text = "Discord"
+    )
+    var supportDiscord: Runnable? =
+        Runnable { browse(URI.create("https://www.polyfrost.cc/discord/")) }
 
     init {
         initialize()
