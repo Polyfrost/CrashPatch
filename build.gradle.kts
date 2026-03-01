ddb962d8ae0390caba48065f17716bf1923ee70f
Marie <80259183+mariethepolarfox@users.noreply.github.com>
2026-03-01 11:55:50 +0100

    publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,16 +5,17 @@ plugins {
     kotlin("jvm") version "2.3.0"
     id("net.fabricmc.fabric-loom-remap") version "1.14-SNAPSHOT"
     id("dev.deftu.gradle.bloom") version "0.2.0"
+    id("me.modmuss50.mod-publish-plugin") version "1.1.0"
 }
 
 val modid = property("mod.id")
 val modname = property("mod.name")
 val modversion = property("mod.version")
-val mcversion = property("minecraft_version")
+val mcversion = stonecutter.current.version
+val oneconfigversion = property("oneconfig_version")
 
-base {
-    archivesName.set(property("mod.id") as String)
-}
+version = "$modversion+$mcversion"
+base.archivesName = modname.toString()
 
 repositories {
     maven("https://maven.parchmentmc.org")
@@ -38,24 +39,19 @@ loom {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:${property("minecraft_version")}")
+    minecraft("com.mojang:minecraft:$mcversion")
     compileOnly("com.mojang:datafixerupper:4.0.26")
     mappings(loom.officialMojangMappings())
 
     modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
-    modImplementation("maven.modrinth:notenoughcrashes:${property("nec_version")}+${property("minecraft_version")}-fabric")
+    modImplementation("maven.modrinth:notenoughcrashes:${property("nec_version")}+$mcversion-fabric")
     implementation(annotationProcessor("com.github.bawnorton.mixinsquared:mixinsquared-common:0.3.3")!!)
     implementation("gs.mclo:api:3.0.1")
 
-    modImplementation("org.polyfrost.oneconfig:${property("minecraft_version")}-fabric:1.0.0-alpha.182")
-    modImplementation("org.polyfrost.oneconfig:commands:1.0.0-alpha.182")
-    modImplementation("org.polyfrost.oneconfig:config:1.0.0-alpha.182")
-    modImplementation("org.polyfrost.oneconfig:config-impl:1.0.0-alpha.182")
-    modImplementation("org.polyfrost.oneconfig:events:1.0.0-alpha.182")
-    modImplementation("org.polyfrost.oneconfig:internal:1.0.0-alpha.182")
-    modImplementation("org.polyfrost.oneconfig:ui:1.0.0-alpha.182")
-    modImplementation("org.polyfrost.oneconfig:utils:1.0.0-alpha.182")
-    modImplementation("org.polyfrost.oneconfig:hud:1.0.0-alpha.182")
+    modImplementation("org.polyfrost.oneconfig:$mcversion-fabric:1.0.0-alpha.182")
+    for (module in arrayOf("config", "config-impl", "internal", "ui")) {
+        implementation("org.polyfrost.oneconfig:$module:$oneconfigversion")
+    }
 }
 
 bloom {
@@ -113,3 +109,30 @@ tasks.jar {
 
 fun <T> optionalProp(property: String, block: (String) -> T?): T? =
     findProperty(property)?.toString()?.takeUnless { it.isBlank() }?.let(block)
+
+val modrinthId = findProperty("publish.modrinth")?.toString()?.takeIf { it.isNotBlank() }
+
+// make sure modrinth.token is set in your user gradle properties
+publishMods {
+    file = project.tasks.remapJar.get().archiveFile
+
+    displayName = modversion.toString()
+    version = "v$modversion"
+    changelog = project.rootProject.file("CHANGELOG.md").takeIf { it.exists() }?.readText() ?: "No changelog provided."
+    type = ALPHA
+
+    modLoaders.add("fabric")
+
+    dryRun = modrinthId == null
+
+    if (modrinthId != null) {
+        modrinth {
+            projectId = property("publish.modrinth").toString()
+            accessToken = findProperty("modrinth.token").toString()
+
+            minecraftVersions.add(mcversion)
+
+            requires("oneconfig")
+        }
+    }
+}
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -2,3 +2,7 @@ plugins {
     id("dev.kikugie.stonecutter")
 }
 stonecutter active "1.21.11"
+
+stonecutter tasks {
+    order("publishModrinth")
+}
